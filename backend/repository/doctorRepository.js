badd8d44252dd8ef7dc548f93e7356b5bfd25581
IPreferWater <ipreferwater2@gmail.com>
2024-02-04 07:43:09 +0100

    feat(mvc): add Dockerfile for api backend

--- backend/repository/doctorRepository.js
+++ backend/repository/doctorRepository.js
@@ -1,12 +1,7 @@
 const Pool = require('pg').Pool
+require('dotenv').config()
 
-const pool = new Pool({
-    user: 'thibaut',
-    host: 'localhost',
-    database: 'yadokari',
-    password: 'password',
-    port: 5432,
-})
+const pool = initPool()
 
 
 function findAllDoctors(callback) {
@@ -43,9 +38,9 @@ function findDoctorsNearby(requestNearbyDoctor , callback) {
 
     const query = {
         text: " SELECT * FROM doctors d LEFT JOIN speciality s ON d.speciality_key = s.speciality_key WHERE 6371 * 2 * ASIN( SQRT(POWER(SIN(RADIANS(d.y - ($1)) / 2), 2) + COS(RADIANS($1)) * COS(RADIANS(d.y)) *POWER(SIN(RADIANS(d.x - ($2)) / 2), 2))) <= ($3) AND s.name = ($4);",
-        values: [requestNearbyDoctor.lat, 
-            requestNearbyDoctor.long, 
-            requestNearbyDoctor.distance, 
+        values: [requestNearbyDoctor.lat,
+            requestNearbyDoctor.long,
+            requestNearbyDoctor.distance,
             requestNearbyDoctor.speciality]
     }
 
@@ -60,6 +55,22 @@ function findDoctorsNearby(requestNearbyDoctor , callback) {
         return callback(null, results.rows);
     })
 
+}
+
+function initPool(){
+    const user = process.env.POSTGRES_USER || "thibaut"
+    const host = process.env.POSTGRES_HOST || "localhost"
+    const database=  process.env.POSTGRES_DB || "yadokari"
+    const password= process.env.POSTGRES_PWD || "password"
+    const port= process.env.POSTGRES_PORT || 5432
+
+    return new Pool({
+        user: user ,
+        host: host,
+        database: database,
+        password: password,
+        port: port,
+    })
 }
 
 
