a18fb5e91abd6a28a19e0e9914d33149474c7e42
Thibaut Roland <thibaut.roland2@gmail.com>
2024-02-07 19:33:56 +0100

    added cors

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,21 +1,45 @@
 
 type IndexProps = {
-  posts: any
+  histories: any
+  d: any
 }
 
-export default function Home({ posts }:IndexProps) {
+export default function Home({ histories, d }:IndexProps) {
 
-  console.log(posts)
+  //console.log(histories)
+  console.log(d)
+
+  async function searchDoctor(doctorName:string){
+    console.log(doctorName)
+   // const res = await fetch(`http://127.0.0.1:3001/doctors/${doctorName}`)
+
+    const res = await fetch(`http://localhost:3001/doctors/${doctorName}`, {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "no-cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": 'true',
+      }
+    });
+    console.log(res)
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       <div className="flex flex-col">
 
-      {posts.map((d: any) => (
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            
+            onClick = { () => searchDoctor('Doctor8') }>OU C KIL EST</button>
+         
+
+      {histories.map((d: any) => (
 
             <div key = {d.id}>
-                {d.name}
+                {d.doctorName} - {d.dateSearched}
             </div>
             )) }
 
@@ -28,14 +52,19 @@ export default function Home({ posts }:IndexProps) {
 
 export async function getServerSideProps() {
 
-  const res = await fetch('http://localhost:3001/doctors')
-  const posts = await res.json()
+  const res = await fetch('http://localhost:3001/doctors/history')
+  const histories = await res.json()
 
-  console.log("from getStatic" +posts)
+  //console.log("from getServersideprops" +histories)
+
+  const resD = await fetch('http://localhost:3001/doctors/Doctor6')
+  const resDJ = await resD.json()
+  console.log("from getServersideprops search docteur" +resDJ)
 
   return {
     props: {
-      posts,
+      histories,
+      d: resDJ
     },
   }
 }
